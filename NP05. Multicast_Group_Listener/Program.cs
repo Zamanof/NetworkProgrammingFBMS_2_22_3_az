7a9343b79b02339c8dd58287e6dd6abb0457e0e7
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2023-12-07 11:37:03 +0400

    NP 05.Multicast

---
+++ NP05. Multicast_Group_Listener/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var listener = new UdpClient(27001);
+var ip = IPAddress.Parse("224.1.2.3");
+listener.JoinMulticastGroup(ip);
+
+var endPoint = new IPEndPoint(IPAddress.Any, 0);
+
+while (true)
+{
+    var data = listener.Receive(ref endPoint);
+    Console.WriteLine(Encoding.Default.GetString(data));
+    Thread.Sleep(40);
+    Console.Clear();
+}
---
+++ NP05. Multicats_Group_Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var client = new UdpClient();
+var ip = IPAddress.Parse("224.1.2.3");
+client.JoinMulticastGroup(ip);
+var endPoint = new IPEndPoint(ip, 27001);
+List<string> strings = new List<string>() {
+    @"\_________",
+    @"/\________",
+    @"_/\_______",
+    @"__/\______",
+    @"___/\_____",
+    @"____/\____",
+    @"_____/\___",
+    @"______/\__",
+    @"_______/\_",
+    @"________/\",
+    @"_________/"
+
+};
+var i = 0;
+var length = strings.Count;
+while (true)
+{
+    var  data = Encoding.Default.GetBytes(strings[i++ % length]);
+    client.Send(data, data.Length, endPoint);
+    Thread.Sleep(40);
+}
