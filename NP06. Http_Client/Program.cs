3e87382bbfb2aa4c1033fbb7cb325b02e08c602e
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2023-12-10 09:22:23 +0400

    some change

--- NP06. Http_Client/Program.cs
+++ NP06. Http_Client/Program.cs
@@ -22,5 +22,5 @@ var responce = await client.SendAsync(message);
 //Console.WriteLine(responce.Headers);
 //Console.WriteLine(responce.StatusCode);
 //Console.WriteLine(responce.Content);
-//Console.WriteLine(responce.RequestMessage);
+Console.WriteLine(responce.RequestMessage);
 
